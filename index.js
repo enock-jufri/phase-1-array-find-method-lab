8957ecf406f0339e30b0a5e9d6fc42d0264f438f
enock-jufri <enockjufri@gmail.com>
2024-10-31 16:41:34 +0300

    find function

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record){
+    const match = record.find(entry=>entry.result === 'W')
+    if(match){
+        return match.year
+    }else
+    return match
+}
